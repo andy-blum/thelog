c8353d5a255840af127d91f1f78b6007b7a881ac
Andy Blum <andy.blum.01@gmail.com>
2021-06-11 23:29:11 -0400

    Filter and round stats for better viewing

--- scraper/src/getPlayers.js
+++ scraper/src/getPlayers.js
@@ -11,6 +11,23 @@ export const getPlayers = async (client, leagueYear) => {
   console.log(`    Found ${fetchedPlayers.length} players.`);
 
   const players = {}
+  const relevantStats = [
+    'receivingYards',
+    'receivingTouchdowns',
+    'receivingReceptions',
+    'defensiveSacks',
+    'madeFieldGoalsFrom50Plus',
+    'madeFieldGoalsFrom40To49',
+    'madeFieldGoalsFromUnder40',
+    'missedFieldGoals',
+    'madeExtraPoints',
+    'missedExtraPoints',
+    'passingYards',
+    'passingTouchdowns',
+    'passingInterceptions',
+    'rushingYards',
+    'rushingTouchdowns',
+  ];
 
   for (const { player, rawStats, projectedRawStats } of fetchedPlayers) {
 
@@ -35,8 +52,21 @@ export const getPlayers = async (client, leagueYear) => {
 
     player.eligiblePositions = filteredPositions;
 
-    delete rawStats.usesPoints;
-    delete projectedRawStats.usesPoints;
+    for (const statName in projectedRawStats) {
+      if (!relevantStats.includes(statName)) {
+        delete projectedRawStats[statName]
+      } else {
+        projectedRawStats[statName] = Math.round(projectedRawStats[statName]);
+      }
+    }
+
+    for (const statName in rawStats) {
+      if (!relevantStats.includes(statName)) {
+        delete rawStats[statName]
+      } else {
+        rawStats[statName] = Math.round(rawStats[statName]);
+      }
+    }
 
     players[player.id] = {
       player,
@@ -48,6 +78,7 @@ export const getPlayers = async (client, leagueYear) => {
 
     players[player.id].player['rookie'] = true;
   }
+
 
   for (let year = (leagueYear.id - 1); year > 2017; year--) {
     console.log(`    Fetching league year ${year}`);
