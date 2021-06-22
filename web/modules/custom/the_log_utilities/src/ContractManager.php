feff75a926914370b3f7b658b8fcd4a114dc2bf0
Andy Blum <andy.blum.01@gmail.com>
2021-06-22 10:19:09 -0400

    Fix cap calculations

--- web/modules/custom/the_log_utilities/src/ContractManager.php
+++ web/modules/custom/the_log_utilities/src/ContractManager.php
@@ -23,11 +23,13 @@ class ContractManager {
     $query->condition('node.nid', $id, '=');
     $query->join('node__field_salary', 'salary', 'node.nid = salary.entity_id');
     $query->join('node__field_years_remaining', 'years', 'node.nid = years.entity_id');
-    $query->join('node_revision__field_player', 'player', 'node.nid = player.entity_id');
+    $query->join('node__field_status', 'status', 'node.nid = status.entity_id');
+    $query->join('node__field_player', 'player', 'node.nid = player.entity_id');
     $query->fields('node', ['nid', 'status', 'title']);
     $query->fields('salary', ['field_salary_value']);
     $query->fields('years', ['field_years_remaining_value']);
     $query->fields('player', ['field_player_target_id']);
+    $query->fields('status', ['field_status_value']);
     $query->range(0, 1);
     $results = $query->execute()->fetchAll();
 
@@ -41,11 +43,13 @@ class ContractManager {
     $query->condition('node.uid', $uid, '=');
     $query->join('node__field_salary', 'salary', 'node.nid = salary.entity_id');
     $query->join('node__field_years_remaining', 'years', 'node.nid = years.entity_id');
-    $query->join('node_revision__field_player', 'player', 'node.nid = player.entity_id');
+    $query->join('node__field_status', 'status', 'node.nid = status.entity_id');
+    $query->join('node__field_player', 'player', 'node.nid = player.entity_id');
     $query->fields('node', ['nid', 'status', 'title']);
     $query->fields('salary', ['field_salary_value']);
     $query->fields('years', ['field_years_remaining_value']);
     $query->fields('player', ['field_player_target_id']);
+    $query->fields('status', ['field_status_value']);
     $results = $query->execute()->fetchAll();
 
     return $results;
@@ -57,16 +61,46 @@ class ContractManager {
 
     $total_salary = 0;
     $total_years = 0;
+    $active_contracts = 0;
+    $dts_contracts = 0;
 
     foreach ($contracts as $contract) {
-      $total_salary += $contract->field_salary_value;
-      $total_years += $contract->field_years_remaining_value;
+
+      if ($contract->field_years_remaining_value > 0) {
+        switch ($contract->field_status_value) {
+          case 'active':
+            $total_salary += $contract->field_salary_value;
+            $total_years += $contract->field_years_remaining_value;
+            $active_contracts ++;
+            break;
+
+          case 'dts':
+            $total_salary += ($contract->field_salary_value / 10);
+            $dts_contracts ++;
+            break;
+
+          case 'ir':
+            $total_salary += ($contract->field_salary_value / 2);
+            $total_years += ($contract->field_years_remaining_value - 1);
+            break;
+
+          case 'waived':
+            $total_salary += ($contract->field_salary_value / 2);
+            $total_years ++;
+            break;
+
+          default:
+            break;
+        }
+      }
+
     }
 
     return [
       'salary' => $total_salary,
       'years' => $total_years,
-      'contracts' => count($contracts)
+      'contracts' => $active_contracts,
+      'dts' => $dts_contracts,
     ];
   }
 
@@ -76,6 +110,7 @@ class ContractManager {
     $user->field_total_contracts->set(0, strval($values['contracts']));
     $user->field_total_contract_years->set(0, strval($values['years']));
     $user->field_total_salary->set(0, strval($values['salary']));
+    $user->field_dts_slots->set(0, strval($values['dts']));
     $user->save();
   }
 }
