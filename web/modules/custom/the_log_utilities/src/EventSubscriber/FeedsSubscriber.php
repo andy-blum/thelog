0541b07d76264dbd4c2b93e7d60278102982f494
Andy Blum <andy.blum.01@gmail.com>
2022-06-27 13:19:53 -0400

    2022 offseason update

--- web/modules/custom/the_log_utilities/src/EventSubscriber/FeedsSubscriber.php
+++ web/modules/custom/the_log_utilities/src/EventSubscriber/FeedsSubscriber.php
@@ -10,30 +10,137 @@ namespace Drupal\the_log_utilities\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\feeds\Event\FeedsEvents;
 use Drupal\feeds\Event\ImportFinishedEvent;
-use Drupal\node\Entity\Node;
+use Drupal\feeds\Event\InitEvent;
+use Drupal\feeds\Event\ParseEvent;
+use Drupal\the_log_utilities\ContractManager;
 
 class FeedsSubscriber implements EventSubscriberInterface {
 
-  public function onImportFinished(ImportFinishedEvent $event) {
-
+  /**
+   * Runs before the feed starts.
+   *
+   * - Deletes waived contracts.
+   *
+   * @param [type] $event
+   * @return void
+   */
+  public function beforeImportStart(InitEvent $event) {
     $type = $event->getFeed()->getType()->id();
+    $stage = $event->getStage();
 
-    if ($type === 'contracts') {
-      $query = \Drupal::entityQuery('node')
+    if ($type === 'contracts' && $stage === 'fetch') {
+      // Delete all waived contracts
+      $nids = \Drupal::entityQuery('node')
         ->condition('type', 'contract')
-        ->condition('field_status', 'waived');
-
-      $nids = $query->execute();
+        ->condition('field_status', 'waived')
+        ->execute();
 
       foreach ($nids as $nid) {
-        $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
-        $node->delete();
+        \Drupal::entityTypeManager()
+          ->getStorage('node')
+          ->load($nid)->delete();
+      }
+    }
+  }
+
+  public function afterParse(ParseEvent $event) {
+
+    $type = $event->getFeed()->getType()->id();
+    $result = $event->getParserResult();
+
+    if ($type === 'contracts' && $result) {
+      for ($i = 0; $i < $result->count(); $i++) {
+        if (!$result->offsetExists($i)) {
+          break;
+        }
+
+        /** @var \Drupal\feeds\Feeds\item\ItemInterface $item */
+        $item = $result->offsetGet($i);
+        $data = $item->toArray();
+
+        $nid = $data['nid'];
+        $salary = intval($data['salary']);
+        $years = intval($data['years']);
+        $status = $data['status'];
+
+        // All contracts decrement by 1 year
+        $years --;
+
+        // 0-year contracts go to RFA
+        if (!$years && $status !== 'dts') {
+          $status = 'rfa';
+        }
+
+        $logger = \Drupal::logger('Contract Updates');
+
+        switch ($status) {
+          case 'active':
+            $salary = round($salary * 1.1);
+            break;
+
+          case 'dts':
+            // TODO: flag top-level DTS contractsx
+            break;
+
+          case 'ir':
+            $status = 'active';
+            $salary = round($salary * 1.1);
+            break;
+
+          case 'waived':
+            $result->offsetUnset($i);
+            $i--;
+            break;
+
+          case 'rfa':
+            break;
+
+          default:
+            $logger->error('Contract @nid has an impossible contract status: @status', [
+              '@nid' => $nid,
+              '@status' => $status
+            ]);
+            break;
+        }
+
+        // Convert cents to dollars
+        $salary = $salary / 100;
+
+        $item->set('years', $years);
+        $item->set('salary', $salary);
+        $item->set('status', $status);
+      }
+    }
+
+  }
+
+  /**
+   * Runs after the feed completes.
+   *
+   * - Updates all cap values
+   *
+   * @param ImportFinishedEvent $event
+   * @return void
+   */
+  public function onImportFinished(ImportFinishedEvent $event) {
+    $uids = \Drupal::entityQuery('user');
+
+    foreach ($uids as $uid) {
+      $user = \Drupal::entityTypeManager()->getStorage('user')->load($uid);
+      if (is_a($user, 'Drupal\user\Entity\User')) {
+        ContractManager::updateCapHit($user);
       }
     }
   }
 
   public static function getSubscribedEvents() {
-    $events[FeedsEvents::IMPORT_FINISHED][] = ['onImportFinished'];
+    $events = [
+      FeedsEvents::INIT_IMPORT => 'beforeImportStart',
+      FeedsEvents::IMPORT_FINISHED => 'onImportFinished',
+    ];
+
+    $events[FeedsEvents::PARSE][] = ['afterParse', FeedsEvents::AFTER];
+
     return $events;
   }
 }
